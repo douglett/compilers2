ebe0f73026fdf5ca3ade330330d6f86a340d5c54
doug@luna <doug@totem9.net>
2018-10-21 22:26:53 +0100

    stuff

--- dbnode/include/parser_expression.hpp
+++ dbnode/include/parser_expression.hpp
@@ -9,8 +9,6 @@ using namespace std;
 class Parser_Expression {
 public:
 	typedef   vector<string>::iterator  vs_iter;
-	vector<string> tok;
-	int pos = 0;
 
 	int parse(Node& n, const vs_iter begin, const vs_iter end) {
 		return parse( n, vector<string>( begin, end ) );
@@ -24,6 +22,35 @@ public:
 			n.kids.push_back({ s });
 		return 0;
 	}
+
+
+private:
+	vector<string> tok;
+	int pos = 0;
+
+	int expect(const string& type, const string& val="") {
+		return 0;
+	}
+	
+	// int parse_expression() {
+	// 	// add or sub
+	// 	if (expect("operator", "+") || expect("operator", "-")) {
+	// 		auto& op = lasttok();
+	// 		parse_term();
+	// 		emit({ "OPR", op.val });
+	// 	}
+	// 	// none - just parse term
+	// 	else {
+	// 		parse_term();
+	// 	}
+	// 	// if add or sub, loop
+	// 	while (expect("operator", "+") || expect("operator", "-")) {
+	// 		auto& op = lasttok();
+	// 		parse_term();
+	// 		emit({ "OPR", op.val });
+	// 	}
+	// }
+
 };
 
 } // end dbas
