c1f129e471808ad9820665a6c98488f9ccaf08ab
Douglas Inman <Douglas.Inman@strategic-i.com>
2018-10-22 16:19:39 +0100

    tokenize string wip

--- dbnode/include/parser_tokenize.hpp
+++ dbnode/include/parser_tokenize.hpp
@@ -15,15 +15,22 @@ public:
 		static const vector<string> TEST_CASES = {
 			"1+2 = 3+a + (12)",
 			"const a = 1"
-			// "a = \"hello world\""
+			"a = \"hello world\""
 		};
-		for (const auto& test : TEST_CASES) {
-			printf("parsing: %s\n", test.c_str());
-			for (const auto& s : parse(test))
-				printf("[%s] ", s.c_str());
-			printf("\n");
+		try {
+			for (const auto& test : TEST_CASES) {
+				printf("parsing: %s\n", test.c_str());
+				for (const auto& s : parse(test))
+					printf("[%s] ", s.c_str());
+				printf("\n");
+			}
+			return 0;
 		}
-		return 0;
+		catch (const string& err) {
+			cerr << "tokenize test error:" << endl
+				<< "\t" << err << endl;
+			return 1;
+		}
 	}
 
 private:
@@ -44,6 +51,11 @@ private:
 				if (s.length())  vs.push_back(s), s = "";
 				vs.push_back(string() + str[i]);
 			}
+			// strings
+			// else if (str[i] == '"') {
+			// 	if (s.length())  vs.push_back(s), s = "";
+			// 	split_string();
+			// }
 			// continue current word
 			else
 				s += str[i];
@@ -51,6 +63,12 @@ private:
 		if (s.length()) vs.push_back(s);
 		return vs;
 	}
+
+	// string splitting
+	int split_string() {
+		throw string("string splitting unimplemented");
+	}
+
 };
 
 } // end dbas
